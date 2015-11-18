bed13b83b94e324f99075664c6c6b8f959950402
ghfan <gavintofan@gmail.com>
2015-11-18 10:20:51 +0800

    problem 26 fibonacci sequence

---
+++ src/26.FibonacciSequence.clj
@@ -0,0 +1,15 @@
+;; Write a function which returns the first X fibonacci numbers.
+
+(= (#(loop [a 1 b 1 n 0 tmp []] (if (= n %) tmp (recur b (+ a b) (inc n) (conj tmp a)))) 3) '(1 1 2))
+
+(= (#(loop [a 1 b 1 n 0 tmp []] (if (= n %) tmp (recur b (+ a b) (inc n) (conj tmp a)))) 6) '(1 1 2 3 5 8))
+
+(= (#(loop [a 1 b 1 n 0 tmp []] (if (= n %) tmp (recur b (+ a b) (inc n) (conj tmp a)))) 8) '(1 1 2 3 5 8 13 21))
+
+;; malvert's solution:
+
+(fn [n]
+  (take n
+    (map first
+      (iterate (fn [[cur prev]] [(+ cur prev) cur]) [1 0])
+     )))
