ee2df828efa6da34b595e6979d0ba984c36288f7
ghfan <gavintofan@gmail.com>
2016-06-13 14:31:11 +0800

    p21 add best soultion

--- src/21.NthElement.clj
+++ src/21.NthElement.clj
@@ -7,3 +7,12 @@
 (= (#(first (subvec (vec %1) %2 (+ 1 %2))) [1 2 3 4] 1) 2)
 
 (= (#(first (subvec (vec %1) %2 (+ 1 %2))) '([1 2] [3 4] [5 6]) 2) [5 6])
+
+;; caterpillar's solution
+(= (#(first (drop %2 %1)) '(4 5 6 7) 2) 6)
+
+;; malvert's solution
+(= ((fn [seq n]
+     (if (= n 0) (first seq)
+		   (recur (rest seq) (dec n))))
+   '(4 5 6 7) 2) 6)
