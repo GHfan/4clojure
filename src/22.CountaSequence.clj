0718314853f41517182a102dd9449a2f8bf700ec
ghfan <gavintofan@gmail.com>
2016-06-13 17:21:43 +0800

    p22 add best solution

--- src/22.CountaSequence.clj
+++ src/22.CountaSequence.clj
@@ -9,3 +9,9 @@
 (= ((fn [s] (loop [s s n 0] (if (empty? s) n (recur (rest s) (+ n 1))))) '(13)) 1)
 
 (= ((fn [s] (loop [s s n 0] (if (empty? s) n (recur (rest s) (+ n 1))))) '(:a :b :c)) 3)
+
+;; malvert's solution
+(= (#(reduce + (map (constantly 1) %)) '(1 2 3 3 1)) 5)
+
+;; zoltanjarai's solution
+(= ((fn ! [s] (if (empty? s) 0 (+ 1 (! (rest s))))) '(1 2 3 3 1)) 5)
