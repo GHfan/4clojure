adf4995bfc993d7109554a002e2f8ab094e7c5c8
ghfan <gavintofan@gmail.com>
2016-06-13 17:48:14 +0800

    p26 add best solutioni

--- src/26.FibonacciSequence.clj
+++ src/26.FibonacciSequence.clj
@@ -7,9 +7,12 @@
 (= (#(loop [a 1 b 1 n 0 tmp []] (if (= n %) tmp (recur b (+ a b) (inc n) (conj tmp a)))) 8) '(1 1 2 3 5 8 13 21))
 
 ;; malvert's solution:
-
 (fn [n]
   (take n
     (map first
-      (iterate (fn [[cur prev]] [(+ cur prev) cur]) [1 0])
+      (iterate (fn [[cur prev]] [(+ cur prev) cur])
+			 [1 0])
      )))
+
+;; caterpillar's solution
+(= (#(loop [i % tmp '(1 1)] (if (= i 2) (reverse tmp) (recur (dec i) (conj tmp (+ (first tmp) (second tmp)))))) 3) '(1 1 2))
