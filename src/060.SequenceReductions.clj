27719b213ccf2c28e7966e2961c3c78de58b8053
yaiba <4yaiba@gmail.com>
2017-05-13 17:51:00 +0800

    p60 sequence reductions

---
+++ src/060.SequenceReductions.clj
@@ -0,0 +1,14 @@
+(fn sr
+  ([f coll]
+   (sr f (first coll) (rest coll)))
+  ([f v coll]
+   (lazy-seq 
+    (if (empty? coll)
+     (vector v)
+     (cons v (sr f (f v (first coll)) (rest coll)))))))
+
+;; (fn [a b & [c]]
+;;   (let [r (if (nil? c) b (cons b c))]
+;;     (map
+;;       #(reduce a (take % r))
+;;       (map #(+ 1 %2) r (range)))))
